785344d6e0215f2746ebb85240bd1223f5b60012
AidynbekZh <aidinbek467@gmail.com>
2023-12-13 01:35:05 +0600

    Добавьте файлы проекта.

---
+++ NoteApp/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+class TaskManager
+{
+    class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    static List<Task> tasks = new List<Task>();
+    static int currentId = 1;
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Менеджер задач:\n" +
+                              "1. Добавить задачу\n" +
+                              "2. Показать все задачи\n" +
+                              "3. Удалить задачу\n" +
+                              "4. Завершить задачу\n" +
+                              "5. Выход");
+
+            int choice = GetChoice(1, 5);
+
+            switch (choice)
+            {
+                case 1:
+                    AddTask();
+                    break;
+                case 2:
+                    ShowTasks();
+                    break;
+                case 3:
+                    DeleteTask();
+                    break;
+                case 4:
+                    CompleteTask();
+                    break;
+                case 5:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+
+    static void AddTask()
+    {
+        Console.Write("Введите название задачи: ");
+        string name = Console.ReadLine();
+
+        Task newTask = new Task { Id = currentId++, Name = name, IsCompleted = false };
+        tasks.Add(newTask);
+
+        Console.WriteLine("Задача добавлена.");
+    }
+
+    static void ShowTasks()
+    {
+        Console.WriteLine("Список задач:");
+
+        foreach (var task in tasks)
+        {
+            string status = task.IsCompleted ? "[Выполнено] " : "";
+            Console.WriteLine($"{task.Id}. {status}{task.Name}");
+        }
+    }
+
+    static void DeleteTask()
+    {
+        Console.Write("Введите ID задачи для удаления: ");
+        int taskId = GetTaskId();
+
+        Task taskToRemove = tasks.Find(t => t.Id == taskId);
+
+        if (taskToRemove != null)
+        {
+            tasks.Remove(taskToRemove);
+            Console.WriteLine("Задача удалена.");
+        }
+        else
+        {
+            Console.WriteLine("Задача не найдена.");
+        }
+    }
+
+    static void CompleteTask()
+    {
+        Console.Write("Введите ID задачи для завершения: ");
+        int taskId = GetTaskId();
+
+        Task taskToComplete = tasks.Find(t => t.Id == taskId);
+
+        if (taskToComplete != null)
+        {
+            taskToComplete.IsCompleted = true;
+            Console.WriteLine("Задача завершена.");
+        }
+        else
+        {
+            Console.WriteLine("Задача не найдена.");
+        }
+    }
+
+    static int GetChoice(int min, int max)
+    {
+        int choice;
+        while (!int.TryParse(Console.ReadLine(), out choice) || choice < min || choice > max)
+        {
+            Console.Write($"Введите корректное значение от {min} до {max}: ");
+        }
+        return choice;
+    }
+
+    static int GetTaskId()
+    {
+        int taskId;
+        while (!int.TryParse(Console.ReadLine(), out taskId) || taskId <= 0)
+        {
+            Console.Write("Введите корректный положительный ID: ");
+        }
+        return taskId;
+    }
+}
+
---
+++ NoteApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("NoteApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoteApp")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("727c2405-7952-4709-9641-c86a6677111c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
